5c1a5701d68267e35fcd7e0ada68694f708c6730
profVitolo <raffaele.vitolo@arrocchi.it>
2026-06-07 13:38:47 +0200

    added 1st real working main

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import subprocess
+import sys
+import webbrowser
+
+
 def main():
-    print("FinanziAI")
-    print("Sorry, not yet implemented")
-    
+    server = subprocess.Popen(
+        [
+            sys.executable,
+            "-m",
+            "uvicorn",
+            "api.app:app"
+        ]
+    )
+
+    webbrowser.open("http://127.0.0.1:8000")
+
+    try:
+        while True:
+            print(" - Write 'exit' to close app - ")
+            command = input("> ").strip().lower()
+
+            if command == "exit":
+                break
+
+    finally:
+        print("Chiusura backend...")
+
+        server.terminate()
+        server.wait()
+
+        print("FinanziAI terminato.")
+
+
 if __name__ == "__main__":
     main()
