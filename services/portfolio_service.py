51832943d78d8242b57a78cfaaa3fee8af4d0548
profVitolo <raffaele.vitolo@arrocchi.it>
2026-06-13 19:26:51 +0200

    Refactored transaction register flow: splitted sell/buy ops with checks

--- services/portfolio_service.py
+++ services/portfolio_service.py
@@ -20,6 +20,55 @@ class PortfolioService:
         if transaction_date is None:
             transaction_date = date.today()
 
+        operation_type = operation_type.lower()
+
+        position = self.portfolio_data_manager.get_position(asset_id)
+
+        if operation_type == "buy":
+            self.portfolio_data_manager.add_transaction(
+                asset_id=asset_id,
+                date=transaction_date,
+                operation_type=operation_type,
+                quantity=quantity,
+                price=price,
+                fees=fees
+            )
+
+            if position is None:
+                self.portfolio_data_manager.update_portfolio_position(
+                    asset_id=asset_id,
+                    quantity=quantity,
+                    avg_price=price,
+                    last_update=transaction_date
+                )
+
+                return
+
+            current_quantity = position[2]
+            current_avg_price = position[3]
+
+            new_quantity = current_quantity + quantity
+
+            new_avg_price = ((current_quantity * current_avg_price) + (quantity * price)) / new_quantity
+
+            self.portfolio_data_manager.update_portfolio_position(
+                asset_id=asset_id,
+                quantity=new_quantity,
+                avg_price=new_avg_price,
+                last_update=transaction_date
+            )
+
+            return
+        # SELL
+        if position is None:
+            raise ValueError("Impossibile vendere un asset non presente in portafoglio")
+
+        current_quantity = position[2]
+        current_avg_price = position[3]
+
+        if quantity > current_quantity:
+            raise ValueError(f"Quantità insufficiente. Disponibili: {current_quantity}")
+
         self.portfolio_data_manager.add_transaction(
             asset_id=asset_id,
             date=transaction_date,
@@ -29,32 +78,16 @@ class PortfolioService:
             fees=fees
         )
 
-        position = self.portfolio_data_manager.get_position(asset_id)
+        new_quantity = current_quantity - quantity
 
-        if position is None:
-            self.portfolio_data_manager.update_portfolio_position(
-                asset_id=asset_id,
-                quantity=quantity,
-                avg_price=price,
-                last_update=transaction_date
-            )
+        if new_quantity == 0:
+            self.portfolio_data_manager.delete_portfolio_position(asset_id)
             return
-
-        current_quantity = position["quantity"]
-        current_avg_price = position["avg_price"]
-
-        if operation_type.lower() == "buy":
-            new_quantity = current_quantity + quantity
-
-            new_avg_price = ((current_quantity * current_avg_price) + (quantity * price) ) / new_quantity
-        else:
-            new_quantity = current_quantity - quantity
-            new_avg_price = current_avg_price
 
         self.portfolio_data_manager.update_portfolio_position(
             asset_id=asset_id,
             quantity=new_quantity,
-            avg_price=new_avg_price,
+            avg_price=current_avg_price,
             last_update=transaction_date
         )
         
