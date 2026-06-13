3fd4e93a849110a013e2fd0a204ee255491e7c86
profVitolo <raffaele.vitolo@arrocchi.it>
2026-06-13 11:50:49 +0200

    First commit

---
+++ api/routes/info.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Request
+
+router = APIRouter(
+    prefix="/info",
+    tags=["Info"]
+)
+
+@router.get("")
+def get_info(request: Request):
+    app = request.app
+
+    return {
+        "application": app.title,
+        "description": app.description,
+        "version": app.version
+    }
