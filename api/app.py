ecefbc5ba1a51a70e5c3dfc3f07d8e85986f0580
profVitolo <raffaele.vitolo@arrocchi.it>
2026-06-15 12:42:20 +0200

    Added transaction's routes

--- api/app.py
+++ api/app.py
@@ -6,6 +6,7 @@ from api.routes.assets import router as assets_router
 from api.routes.analysis import router as analysis_router
 from api.routes.portfolio import router as portfolio_router
 from api.routes.info import router as info_router
+from api.routes.transaction import router as transaction_router
 
 app = FastAPI(
     title="FinanziAI",
@@ -17,6 +18,7 @@ app.include_router(assets_router)
 app.include_router(analysis_router)
 app.include_router(portfolio_router)
 app.include_router(info_router)
+app.include_router(transaction_router)
 
 app.add_middleware(
     CORSMiddleware,
