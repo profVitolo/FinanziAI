1b28492368fb376d6807adc4ac45fb9a48848a33
profVitolo <raffaele.vitolo@arrocchi.it>
2026-06-17 13:11:46 +0200

    Change roadmap and introduced 0.1.1 ver

--- api/app.py
+++ api/app.py
@@ -11,7 +11,7 @@ from api.routes.transaction import router as transaction_router
 app = FastAPI(
     title="FinanziAI",
     description="AI-Assisted Investment Analysis",
-    version="0.1.0"
+    version="0.1.1"
 )
 
 app.include_router(assets_router)
