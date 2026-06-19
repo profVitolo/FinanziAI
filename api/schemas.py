8922d88cb6028e8663c13ae5caa315be0a0253e1
profVitolo <raffaele.vitolo@arrocchi.it>
2026-06-19 12:30:48 +0200

    added exchange schemas

--- api/schemas.py
+++ api/schemas.py
@@ -1,5 +1,7 @@
 from datetime import date
 from pydantic import BaseModel, Field
+from fastapi import Query
+
 
 class TransactionCreate(BaseModel):
     asset_id: int
@@ -20,3 +22,42 @@ class TransactionsFilter(BaseModel):
     start_date: date | None = None
     end_date: date | None = None
     asset_id: int | None = None
+
+
+class ExchangeRateSync(BaseModel):
+    from_currency: str
+    to_currency: str
+    rate_date: str | None = None
+
+class ExchangeRatesSync(BaseModel):
+    from_currency: str
+    to_currency: str
+    start_date: str
+    end_date: str | None = None
+    
+class ExchangeConvert(BaseModel):
+    amount: float
+    from_currency: str
+    to_currency: str
+    rate_date: str | None = None
+    
+
+class ExchangeRatesFilter:
+    def __init__(
+        self,
+        from_currency: str | None = Query(None),
+        to_currency: str | None = Query(None),
+        start_date: str | None = Query(None),
+        end_date: str | None = Query(None)
+    ):
+        self.from_currency = from_currency
+        self.to_currency = to_currency
+        self.start_date = start_date
+        self.end_date = end_date
+
+class MissingDatesFilter:
+    def __init__(self, from_currency: str, to_currency: str, start_date: str, end_date: str | None = None):
+        self.from_currency = from_currency
+        self.to_currency = to_currency
+        self.start_date = start_date
+        self.end_date = end_date
