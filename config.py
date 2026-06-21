30b2305715d0440de706137267270a2d9c205e1a
profVitolo <raffaele.vitolo@arrocchi.it>
2026-06-21 13:19:17 +0200

    Added bootstrap_days const (90days): number of days to first download of a new asset

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ ROOT_DIR = Path(__file__).resolve().parent
 DB_PATH = ( ROOT_DIR / "database" / "vault.db" )
 SCHEMA_PATH = (ROOT_DIR / "database" / "init_db.sql" )
 BASE_CURRENCY = "EUR"
+BOOTSTRAP_DAYS = 90
