9e34a78111adff4451989b4d8ff6338c259a7942
profVitolo <raffaele.vitolo@arrocchi.it>
2026-06-24 13:02:44 +0200

    Added database service endpoints

--- api/routes/info.py
+++ api/routes/info.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Request
 from config import BASE_CURRENCY
 
+from services.database_service import DatabaseService
+from api.schemas import DatabaseRequest
+
 router = APIRouter(prefix="/info", tags=["Info"])
 
 @router.get("")
@@ -12,4 +15,30 @@ def get_info(request: Request):
         "description": app.description,
         "version": app.version,
         "base_currency": BASE_CURRENCY
+    }
+    
+@router.get("/databases")
+def get_databases():
+    service = DatabaseService()
+
+    return {
+        "selected": service.get_current_vault(),
+        "databases": service.list_vaults()
+    }
+    
+@router.post("/database/create")
+def create_database(request: DatabaseRequest):
+    service = DatabaseService()
+
+    return {
+        "database": service.create_vault(request.db_name)
+    }
+    
+@router.post("/database/select")
+def select_database(request: DatabaseRequest):
+    service = DatabaseService()
+    service.set_vault(request.db_name)
+
+    return {
+        "database": service.get_current_vault()
     }
