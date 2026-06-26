c2210e6754372c54ae60a21fd6e7cf473b9ee454
profVitolo <raffaele.vitolo@arrocchi.it>
2026-06-26 12:59:20 +0200

    First commit fo base rules class

---
+++ advisor/advisor_base_rules.py
@@ -0,0 +1,44 @@
+from advisor.advisor_models import Severity, AdvisorSummary, AdvisorMessage
+
+
+class AdvisorBaseRules:
+    @classmethod
+    def _highest_severity(cls, messages):
+        if not messages:
+            return None
+
+        priority = {
+            Severity.LOW: 1,
+            Severity.MEDIUM: 2,
+            Severity.HIGH: 3
+        }
+
+        return max(
+            (message.severity for message in messages),
+            key=lambda severity: priority[severity]
+        )
+    
+    @classmethod
+    def _build_summary(cls, messages):
+        return AdvisorSummary(
+            message_count=len(messages),
+            highest_severity=cls._highest_severity(messages)
+        )
+    
+    @classmethod
+    def collect_messages(cls, *messages):
+        return [message for message in messages if message is not None]
+    
+    @classmethod
+    def message(cls, code, type, severity, message):
+        return AdvisorMessage(
+            code=code,
+            type=type,
+            severity=severity,
+            message=message
+        )
+    
+    @classmethod
+    def build_result(cls, result_class, **kwargs):
+        kwargs["summary"] = cls._build_summary(kwargs["messages"])
+        return result_class(**kwargs)
